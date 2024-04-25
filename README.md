b3950fe4184a33a86c9766cd9a96a05f20f02ef1
Lushito <luisdevcol@gmail.com>
2024-04-24 20:44:49 -0500

    [ADD]: finished proyect cat

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# React + Vite
+# Prueba tecnica junior trainee
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+APIS : 
+- https://catfact.ninja/Facts
+- https://cataas.com/cat/says/holi
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+[x] Recupera un texto aleatorio de gatos de la primera api
+[x] Recuperar la primera palabra del texto 
+[x] Mostrar una imagen de un gato con la palabra
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from "react";
-import "./App.css";
+import  { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
+export const App = () => {
+  const [fact, setFact] = useState('loreum ipsum catum dogum')
+  const [ImageUrl, setImageUrl] = useState()
+  const CAT_API = 'https://catfact.ninja/fact'
+  const CAT_PREFIX_IMAGE_URL =' https://cataas.com'
+  
+
+  useEffect(() =>{
+    fetch(CAT_API)
+    .then(response => response.json())
+    .then(data => {
+      const { fact } = data
+      setFact(fact)
+      const firstWord = fact.split(' ', 3).join(' ')
+     
+      fetch(`https://cataas.com/cat/says/${firstWord}?size=40&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { _id } = response
+        const url = `/cat/${_id}/says/${firstWord}`
+        setImageUrl(CAT_PREFIX_IMAGE_URL + url)
+      })
+    })
+  },[]) // cada vez que yo renderizo el componente , quiero que se ejecute esto 
  
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={ ()=> setCount((count) => count ++)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  );
+    <div>
+      <h1>App de gatos </h1>
+      <h2>{fact}</h2>
+      {ImageUrl && <img src={ImageUrl} alt='cats'/>}
+    </div>
+  )
 }
-
-export default App;
